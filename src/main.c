20f6fee7e630d37fdbf942df5dde05c371cac65b
孙猛 <meng.sun@k7.cn>
2019-08-23 17:29:23 +0800

    Update main.c
    增加打印语句

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
-
+#include "zmalloc.h"
+//int usetcmallocFlag = 0;
+//int usejemallocFlag = 0;
+//int appleFlag = 0;
+//int zmalloclibFlag = 0;
+//int havaMallocSizeFlag = 0;
 int main()
 {
+	printf("usetcmallocFlag:%d\n", usetcmallocFlag);
+	printf("usejemallocFlag:%d\n", usejemallocFlag);
+	printf("appleFlag:%d\n", appleFlag);
+	printf("zmalloclibFlag:%d\n", zmalloclibFlag);
+	printf("havaMallocSizeFlag:%d\n", havaMallocSizeFlag);
 	printf("hello world\n");
 	return 0;
 }
